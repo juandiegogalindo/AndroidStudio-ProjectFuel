4e33409b0ff00a95e68a233b2a42978a48e7ed90
juandiegogalindo <jdgalindo6@gmail.com>
2026-03-05 13:53:48 -0500

    base de datios 5 o 6 para su ejecucion

--- app/src/main/java/co/edu/unipiloto/scrumbacklog/database/DatabaseHelper.java
+++ app/src/main/java/co/edu/unipiloto/scrumbacklog/database/DatabaseHelper.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 public class DatabaseHelper extends SQLiteOpenHelper {
 
     private static final String DATABASE_NAME = "combustible.db";
-    private static final int DATABASE_VERSION = 4;
+    private static final int DATABASE_VERSION = 5;
 
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
